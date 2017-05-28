3ccab1ca6177e963ee5fcf6f6fab4e296fd8a6ca
santicivetta <santiago.civettam@gmail.com>
2017-05-28 15:41:19 -0300

    modifique archivos.java

--- src/cromosomas/Archivos.java
+++ src/cromosomas/Archivos.java
@@ -3,6 +3,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Archivos {
+	
+	//MARCELITO ESTAS AHI?
 
 	public static Integer[] cargarConfiguraciones(String archivo) {
 		BufferedReader br = null;
